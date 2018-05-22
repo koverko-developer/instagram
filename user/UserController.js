6d15ae1bf0e7682e29915a3cdfb8cd311157e593
koverko-developer <kowerkodeveloper@gmail.com>
2018-05-22 13:34:37 +0300

    Add files via upload

--- user/UserController.js
+++ user/UserController.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var Client = require('instagram-private-api').V1;
-const DR = '/root/instagram/client/v1/cookies/'
+const DR = '/home/mobi-app/development/node/instagram/instagram-private-api/client/v1/cookies/'
+
 var fs = require('fs');
 
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -13,18 +14,43 @@ router.post('/get/', function (req, res) {
   var device = new Client.Device(login);
   var storage = new Client.CookieFileStorage(DR+login+'.json');
   var session = new Client.Session(device, storage)
+  var d;
+  var b;
 
-  fs.readFile(DR+login+'.json', "utf8", function(err, data) {
-        
-        if(err) res.send(err);
-        else {
-          console.log(data)
-          res.send(data)  
-        }
-      
-      });
+  var readStream = fs.createReadStream(DR+login+'.json');
+  readStream
+    .on('data', function (chunk) {
+        //res.send(chunk);
+        d = chunk;
+        getAcc(session, d,res, login)
+    })
+    .on('end', function () {
+        // This may not been called since we are destroying the stream
+        // the first time 'data' event is received
+        console.log('All the data in the file has been read');
+        readStream.destroy();
+    })
+    .on('close', function (err) {
+        console.log('Stream has been destroyed and file has been closed');
+    });
+
 });
 
+function getAcc(session, data, res, login) {
+  console.log('get acc');
+  session.getAccount()
+    .then(function(account) {
+      console.log(account.params)
+      b = account.params;
+      fs.writeFile(DR+login+'.json', data , function(err) {
+        if(err) {
+            res.send(err);
+        }else {
+            res.send(account.params)
+        }
+      });
+    })
+}
 
 router.get('/', function (req, res) {
     res.send("get user");
