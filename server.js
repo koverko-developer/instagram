e08d2ba743b7bb90418c188888d6b1c27136f8ea
koverko-developer <kowerkodeveloper@gmail.com>
2018-09-20 23:14:30 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-var app = require('./app');
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var firebase = require('firebase');
+var request = require('request');
+var AuthController = require('./auth/AuthController');
+var UserController = require('./user/UserController');
+
 var port = process.env.PORT || 80;
 
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
+var app = express();
+
+var firebase = require('firebase');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// API routes
+app.use('/api/auth', AuthController);
+app.use('/users/', UserController);
+
+// Running the server
+app.listen(port, () => {
+	console.log(`http://localhost:${port}`)
+})
