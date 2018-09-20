0d93edd6afc16b9550aaebc414c423b1ba5ac327
koverko-developer <kowerkodeveloper@gmail.com>
2018-09-20 23:15:11 +0300

    Add files via upload

--- user/UserController.js
+++ user/UserController.js
@@ -1,75 +1,103 @@
 var express = require('express');
-var router = express.Router();
+var request = require('request');
 var bodyParser = require('body-parser');
-var Client = require('instagram-private-api').V1;
-const DR = '/root/instagram/client/v1/cookies/'
+var firebase = require('firebase');
+var db = require('../db');
+var Error = require('../model/Error');
+var error = new Error();
+var database = firebase.database();
 
-var fs = require('fs');
+var user = express.Router();
+user.use(bodyParser.urlencoded({ extended: true }));
+user.use(bodyParser.json());
 
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
 
-router.post('/get/', function (req, res) {
-  let login = req.body.login;
-  var device = new Client.Device(login);
-  var storage = new Client.CookieFileStorage(DR+login+'.json');
-  var session = new Client.Session(device, storage)
-  var d;
-  var b;
+user.route('/info/')
+    .get((req, res) => {
 
-  var readStream = fs.createReadStream(DR+login+'.json');
-  readStream
-    .on('data', function (chunk) {
-        //res.send(chunk);
-        d = chunk;
-        getAcc(session, d,res, login)
     })
-    .on('end', function () {
-        // This may not been called since we are destroying the stream
-        // the first time 'data' event is received
-        console.log('All the data in the file has been read');
-        readStream.destroy();
+    .post((req, res) => {
+      getUserInfo(req, res);
     })
-    .on('close', function (err) {
-        console.log('Stream has been destroyed and file has been closed');
+user.route('/:id/media/info')
+    .get((req, res) => {
+
+    })
+    .post((req, res) => {
+      chekoutInF(req, res);
+    })
+
+function getUserInfo(req, res) {
+  var token = req.body.access_token;
+  if(!req.body.access_token) token = '123456';
+    console.log(req.body.access_token);
+    request('https://api.instagram.com/v1/users/self/?access_token='+token, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        if(error == null ){
+          var b = JSON.parse(body);
+          if(!b['meta']['error_type']) chekoutInF(req, res, body);
+          else {
+            res.send(body);
+          }
+        }else {
+          res.send(body);
+        }
     });
 
-});
-
-function getAcc(session, data, res, login) {
-  console.log('get acc');
-  session.getAccount()
-    .then(function(account) {
-      console.log(account.params)
-      b = account.params;
-      fs.writeFile(DR+login+'.json', data , function(err) {
-        if(err) {
-            res.send(err);
-        }else {
-            res.send(account.params)
-        }
-      });
-    })
 }
 
-router.get('/', function (req, res) {
-    res.send("get user");
-});
+function chekoutInF(req, res){
 
-// GETS A SINGLE USER FROM THE DATABASE
-router.get('/:id', function (req, res) {
+    let userId = req.params.id;
+    console.log(userId);
+    //console.log(error);
+    firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+      var access_token = (snapshot.val() && snapshot.val().access_token) || 'Anonymous';
+      console.log(access_token);
+      if(access_token === 'Anonymous'){
+          res.send(error);
+      }else{
+          if(!req.body.url) getAllPosts(res, access_token);
+          else getAllPosts(res, access_token, req.body.url);
+      }
+    });
+}
 
-});
+function getAllPosts(res, access_token, url){
+  var start = new Date
+  if(!url) var url =  'https://api.instagram.com/v1/users/self/media/recent/?access_token='+access_token;
+  request(url, function (errors, response, body) {
+        console.log('error:', errors); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //console.log('body:', body); // Print the HTML for the Google homepage.
+        if(errors == null ){
+          var list = JSON.parse(body);
+          var end = new Date;
+          console.log("Цикл занял " + (end - start) + " ms" );
+          console.log(list['pagination']);
+          res.send(list);
 
-// DELETES A USER FROM THE DATABASE
-router.delete('/:id', function (req, res) {
+        }else {
+          console.log(errors);
+          res.send(error);
+        }
+    });
 
-});
+}
 
-// UPDATES A SINGLE USER IN THE DATABASE
-router.put('/:id', function (req, res) {
+function getUserInfo(req, res) {
+  var token = req.body.access_token;
+  if(!req.body.access_token) token = '123456';
+    console.log(req.body.access_token);
+    request('https://api.instagram.com/v1/users/self/?access_token='+token, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        res.send(body);
+    });
 
-});
+}
 
-
-module.exports = router;
+module.exports = user;
